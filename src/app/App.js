c423a7a0a792d05726599d27cd0ae499f12daa2c
Maxim Trekusov <trekusovmax@gmail.com>
2021-12-11 19:15:33 +0300

    react 3.2 done

--- src/app/App.js
+++ src/app/App.js
@@ -4,24 +4,20 @@ import SearchStatus from './components/searchStatus'
 import api from './api'
 
 function App() {
-  const newState = api.users.fetchAll()
-  newState.map((user) => (user.favorite = false))
-
-  const [users, setUsers] = useState(newState)
+  const [users, setUsers] = useState(api.users.fetchAll())
 
   const handleDelete = (userID) => {
     setUsers((prevState) => prevState.filter((user) => user._id !== userID))
   }
 
   const handleToggleBookMark = (id) => {
-    setUsers((prevState) => {
-      return prevState.map((user) => {
-        if (user._id === id) {
-          user.favorite = !user.favorite
-        }
-        return user
-      })
+    const newState = users.map((user) => {
+      if (user._id === id) {
+        user.favorite = !user.favorite
+      }
+      return user
     })
+    setUsers(newState)
   }
 
   return (
@@ -29,26 +25,11 @@ function App() {
       <SearchStatus length={users.length} />
 
       {users.length > 0 && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th scope="col">Избранное</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <Users
-              users={users}
-              onDelete={handleDelete}
-              onToggle={handleToggleBookMark}
-            />
-          </tbody>
-        </table>
+        <Users
+          users={users}
+          handleDelete={handleDelete}
+          handleToggleBookMark={handleToggleBookMark}
+        />
       )}
     </>
   )
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -3,22 +3,35 @@ import User from './user'
 
 const Users = ({ users, ...rest }) => {
   return (
-    <>
-      {users.map((user) => (
-        <User
-          key={user._id}
-          qualities={user.qualities}
-          _id={user._id}
-          name={user.name}
-          profession={user.profession}
-          completedMeetings={user.completedMeetings}
-          rate={user.rate}
-          handleDelete={rest.onDelete}
-          handleToggleBookMark={rest.onToggle}
-          favorite={user.favorite}
-        />
-      ))}
-    </>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Имя</th>
+          <th scope="col">Качества</th>
+          <th scope="col">Профессия</th>
+          <th scope="col">Встретился, раз</th>
+          <th scope="col">Оценка</th>
+          <th scope="col">Избранное</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <User
+            key={user._id}
+            qualities={user.qualities}
+            _id={user._id}
+            name={user.name}
+            profession={user.profession}
+            completedMeetings={user.completedMeetings}
+            rate={user.rate}
+            handleDelete={rest.handleDelete}
+            handleToggleBookMark={rest.handleToggleBookMark}
+            favorite={user.favorite}
+          />
+        ))}
+      </tbody>
+    </table>
   )
 }
 
